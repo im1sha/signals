332a06004d99c7373c962b62b8e2c8cccce7af2f
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-10-14 20:47:47 +0300

    initial implementation

--- SignalsTwo/SignalsTwo/Form1.Designer.cs
+++ SignalsTwo/SignalsTwo/Form1.Designer.cs
@@ -28,13 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(727, 315);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(463, 305);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "64",
+            "128",
+            "256",
+            "512",
+            "1024",
+            "2048"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "64";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            this.chart2.Location = new System.Drawing.Point(727, 33);
+            this.chart2.Name = "chart2";
+            this.chart2.Size = new System.Drawing.Size(463, 276);
+            this.chart2.TabIndex = 5;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            this.chart3.Location = new System.Drawing.Point(186, 34);
+            this.chart3.Name = "chart3";
+            this.chart3.Size = new System.Drawing.Size(535, 275);
+            this.chart3.TabIndex = 6;
+            this.chart3.Text = "chart3";
+            // 
+            // chart4
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea4);
+            this.chart4.Location = new System.Drawing.Point(186, 315);
+            this.chart4.Name = "chart4";
+            this.chart4.Size = new System.Drawing.Size(535, 305);
+            this.chart4.TabIndex = 7;
+            this.chart4.Text = "chart4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "fi=2*pi/3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(362, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "x(n)=sin((2*pi*n)/N)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(846, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "x(n)=sin((2*pi*n)/N+phi)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1205, 632);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- SignalsTwo/SignalsTwo/Form1.cs
+++ SignalsTwo/SignalsTwo/Form1.cs
@@ -1,20 +1,204 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace SignalsTwo
 {
+    //
+    // x(n) = sin(2 * Math.PI * n / N)
+    // N % 64 == 0 
+    // n = {0 .. M}
+    // M 
     public partial class Form1 : Form
     {
+        private Series signal;
+        private Series DataSer_1;
+        private Series DataSer_2;
+        private Series A_1;
+        private Series A_2;
+        private int K;
+        private double phi;
+        private readonly double[] phc = new double[15];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        //private static double DiscreteFourierTransform(double[] signal, int j)
+        //{
+        //    double N = signal.Length;
+        //    double Acj = 2 / N, Asj = 2 / N;
+        //    double sumAcj = 0.0d, sumAsj = 0.0d;
+        //    for (int i = 0; i < N - 1; i++)
+        //    {
+        //        sumAcj += signal[i] * Math.Cos(2 * Math.PI * i * j / N);
+        //        sumAcj += signal[i] * Math.Sin(2 * Math.PI * i * j / N);
+        //    }
+
+        //    Acj *= sumAcj;
+        //    Asj *= sumAsj;
+
+        //    return Math.Sqrt(Math.Pow(Acj, 2) + Math.Pow(Asj, 2));
+        //}
+
+        private void BuildGraphWithoutPhi()
+        {
+            chart3.Series.Clear();
+            chart4.Series.Clear();
+
+            chart3.Legends.Clear();
+            chart4.Legends.Clear();
+            DataSer_1 = new Series
+            {
+                Name = "СКЗ1",
+                ChartType = SeriesChartType.Spline,
+                Color = Color.Blue,
+            };
+            DataSer_2 = new Series
+            {
+                Name = "СКЗ2",
+                ChartType = SeriesChartType.Spline,
+                Color = Color.BlueViolet,
+            };
+            signal = new Series
+            {
+                Name = "X",
+                ChartType = SeriesChartType.Spline,
+                Color = Color.OrangeRed
+            };
+            A_1 = new Series
+            {
+                Name = "A_1",
+                ChartType = SeriesChartType.Spline,
+                Color = Color.Black
+            };
+            A_2 = new Series
+            {
+                Name = "A_2",
+                ChartType = SeriesChartType.Spline,
+                Color = Color.YellowGreen
+            };
+
+            int N = int.Parse(comboBox1.Text);
+            K = N / 4;
+
+            // should be M = N - 1
+            for (int M = K; M <= 2 * N; M++)
+            {
+                double rms_1 = 0, rms_2 = 0, acj = 0, asj = 0;
+                for (int n = 0; n <= M; n++)
+                {
+                    double t = Math.Sin(2 * Math.PI * n / N);
+
+                    rms_1 += Math.Pow(t, 2);
+                    rms_2 += t;
+
+                    acj += t * Math.Sin(2 * Math.PI * n / M);
+                    asj += t * Math.Cos(2 * Math.PI * n / M);
+                }
+                acj = 2 * acj / M;
+                asj = 2 * asj / M;
+
+                signal.Points.AddXY(M, rms_2);
+                DataSer_1.Points.AddXY(M, 0.707 - Math.Sqrt(rms_1 / (M + 1)));
+                DataSer_2.Points.AddXY(M, 0.707 - (Math.Sqrt(rms_1 / (M + 1) - Math.Pow(rms_2 / (M + 1), 2))));
+                A_1.Points.AddXY(M, 1 - Math.Sqrt(acj * acj + asj * asj));
+            }
+            chart3.ResetAutoValues();
+            chart4.ResetAutoValues();
+
+            chart3.Legends.Add(signal.Legend);
+            chart4.Legends.Add(DataSer_1.Legend);
+            chart3.Series.Add(signal);
+            chart4.Series.Add(DataSer_1);
+            chart4.Series.Add(DataSer_2);
+            chart4.Series.Add(A_1);
+            //chart4.Series.Add(A_2);
+        }
+
+        private void BuildGraph()
+        {
+            chart1.Series.Clear();
+            chart2.Series.Clear();
+
+            chart1.Legends.Clear();
+            chart2.Legends.Clear();
+
+            DataSer_1 = new Series
+            {
+                Name = "СКЗ1",
+                ChartType = SeriesChartType.Spline,
+                Color = Color.Blue
+            };
+            DataSer_2 = new Series
+            {
+                Name = "СКЗ2",
+                ChartType = SeriesChartType.Spline,
+                Color = Color.BlueViolet
+            };
+            signal = new Series
+            {
+                Name = "X",
+                ChartType = SeriesChartType.Spline,
+                Color = Color.OrangeRed
+            };
+            A_1 = new Series
+            {
+                Name = "A_1",
+                ChartType = SeriesChartType.Spline,
+                Color = Color.Black
+            };
+            A_2 = new Series
+            {
+                Name = "A_2",
+                ChartType = SeriesChartType.Spline,
+                Color = Color.YellowGreen
+            };
+            int N = int.Parse(comboBox1.Text);
+            K = 3 * N / 4;
+            phi = 120;
+            for (int M = K; M <= 2 * N; M++)
+            {
+                double rms_1 = 0, rms_2 = 0, acj = 0, asj = 0;
+                for (int n = 0; n <= M; n++)
+                {
+                    double t = Math.Sin(2 * Math.PI * n / N 
+                        + phi / 180 * Math.PI);
+                    rms_1 += Math.Pow(t, 2);
+                    rms_2 += t;
+
+                    acj += t * Math.Sin(2 * Math.PI * n / M);
+                    asj += t * Math.Cos(2 * Math.PI * n / M);
+                }
+
+                acj = acj * 2 / M;
+                asj = asj * 2 / M;
+
+                signal.Points.AddXY(M, rms_2);
+                DataSer_1.Points.AddXY(M, 0.707 - Math.Sqrt(rms_1 / (M + 1)));
+                DataSer_2.Points.AddXY(M, 0.707 - (Math.Sqrt(rms_1 / (M + 1) - Math.Pow(rms_2 / (M + 1), 2))));
+                A_1.Points.AddXY(M, 1 - Math.Sqrt(acj * acj + asj * asj));
+                //A_2.Points.AddXY(M, 1 - 0.707 * (Math.Sqrt(rms_1 / (M + 1) - Math.Pow(rms_2 / (M + 1), 2))));
+            }
+            chart1.ResetAutoValues();
+            chart2.ResetAutoValues();
+
+            chart2.Legends.Add(DataSer_1.Legend);
+            chart1.Legends.Add(signal.Legend);
+            chart2.Series.Add(signal);
+            chart1.Series.Add(DataSer_1);
+            chart1.Series.Add(DataSer_2);
+            chart1.Series.Add(A_1);
+            //chart1.Series.Add(A_2);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BuildGraph();
+            BuildGraphWithoutPhi();
         }
     }
 }
